7e33983494f2db288fc953c70093a581e9228138
Fletcher Easton <fletcheaston@gmail.com>
2019-05-27 17:21:37 -0700

    Python sensor reading script has been started.

--- Python/Gripper.py
+++
@@ -1,7 +0,0 @@
-# Created by Fletcher Easton
-
-class Gripper:
-    def __init__(self, x, y):
-        self.x = x;
-        self.y = y;
-        self.plant = None;
--- Python/Position.py
+++
@@ -1,9 +0,0 @@
-# Created by Fletcher Easton
-
-class Position:
-    def __init__(self, x, y):
-        self.x = x;
-        self.y = y;
-
-    def __repr__(self):
-        return("Position(X:{!r}, Y:{!r})".format(self.x, self.y));
