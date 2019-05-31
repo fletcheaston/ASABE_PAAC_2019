a12831dcfe1bf83036184bd6eea3e15fe33d27b6
Fletcher Easton <fletcheaston@gmail.com>
2019-05-31 10:32:07 -0700

    Added an input text test.

---
+++ Python/SSHTest.py
@@ -0,0 +1,27 @@
+import serial
+import time
+from Motors import *
+
+robotSerial = serial.Serial(port="/dev/ttyUSB1", baudrate=57600, timeout=0.1);
+validDirections = ["FORWARD", "BACKWARD", "LEFT", "RIGHT"];
+
+while(True):
+    
+    direction = None;
+    while(True):
+        direction = input("Enter a direction: ").upper();
+        if(direction in validDirections):
+            break;
+        print("Not a valid ")
+    
+    speed = None;
+    while(speed is None):
+        try:
+            speed = int(input("Enter a speed: "));
+        except:
+            print("Not an integer. Try again.");
+            speed = None;
+
+    setDirectionSpeed(robotSerial, direction, speed):
+    
+    time.sleep(0.1);
