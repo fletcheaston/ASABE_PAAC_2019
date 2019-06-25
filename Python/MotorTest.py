9d3c4f8d348227c1f03c8695d5ea45164590935b
Fletcher Easton <fletcheaston@Fletchers-MacBook-Pro.local>
2019-06-25 10:37:13 -0700

    Bug fixes.

--- Python/MotorTest.py
+++ Python/MotorTest.py
@@ -4,25 +4,21 @@ from Motors import *;
 import time;
 import serial;
 
-try:
-    motorSerial = serial.Serial('/dev/tty.wchusbserial1410', 57600, timeout=0.1);
+motorSerial = serial.Serial('/dev/ttyUSB0', 57600, timeout=0.1);
 
-    speed = 255;
+speed = 255;
 
-    time.sleep(5);
+time.sleep(5);
 
-    print("Forward");
-    setDirectionPosition(motorSerial, "FORQARD", 500, 255);
+print("Forward");
+setDirectionPosition(motorSerial, "FORWARD", 500, 255);
 
-    time.sleep(1);
+time.sleep(1);
 
-    motorSerial.write(b"M");
+motorSerial.write(b"M");
 
+time.sleep(0.1);
+
+while(motorSerial.in_waiting > 0):
     time.sleep(0.1);
-
-    while(motorSerial.in_waiting > 0):
-        time.sleep(0.1);
-        print(motorSerial.readline().decode());
-
-except:
-    pass;
+    print(motorSerial.readline().decode());
