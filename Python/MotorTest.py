f5b853c331cd1ed53ba392a06229f33b4fd15817
Fletcher Easton <fletcheaston@gmail.com>
2019-06-25 10:49:41 -0700

    More bug fixes. Continuously prints motor data now. Again...

--- Python/MotorTest.py
+++ Python/MotorTest.py
@@ -19,13 +19,13 @@ time.sleep(1);
 print("Forward");
 setDirectionPosition(motorSerial, "FORWARD", 500, 255);
 
-time.sleep(1);
-
-motorSerial.write(b"M");
-
-time.sleep(0.1);
+time.sleep(5);
 
 while(True):
+    motorSerial.write(b"M");
+
+    time.sleep(0.1);
+
     while(motorSerial.in_waiting > 0):
         time.sleep(0.1);
         print(motorSerial.readline().decode());
