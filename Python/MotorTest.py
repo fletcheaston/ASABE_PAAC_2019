fe86a7df7e98c65b366556d9cbd709a559eb3fca
Fletcher Easton <fletcheaston@Fletchers-MacBook-Pro.local>
2019-06-25 10:34:31 -0700

    Motor updates for testing.

--- Python/MotorTest.py
+++ Python/MotorTest.py
@@ -6,72 +6,23 @@ import serial;
 
 try:
     motorSerial = serial.Serial('/dev/tty.wchusbserial1410', 57600, timeout=0.1);
-    
+
     speed = 255;
 
     time.sleep(5);
 
+    print("Forward");
+    setDirectionPosition(motorSerial, "FORQARD", 500, 255);
 
-    while(True):
-        print("Forward");
-        setDirectionSpeed(motorSerial, "FORWARD", speed);
-        
-        time.sleep(1);
+    time.sleep(1);
 
-        motorSerial.write(b"M");
-        
-        time.sleep(0.1);
-        
-        while(motorSerial.in_waiting > 0):
-            time.sleep(0.1);
-            print(motorSerial.readline().decode());
+    motorSerial.write(b"M");
 
-        time.sleep(10);
+    time.sleep(0.1);
 
-        print("Backward");
-        setDirectionSpeed(motorSerial, "BACKWARD", speed);
-        
-        time.sleep(1);
-        
-        motorSerial.write(b"M");
-                
+    while(motorSerial.in_waiting > 0):
         time.sleep(0.1);
-        
-        while(motorSerial.in_waiting > 0):
-            time.sleep(0.1);
-            print(motorSerial.readline().decode());
-            
-        time.sleep(10);
-        
-        print("Left");
-        setDirectionSpeed(motorSerial, "LEFT", speed);
-        
-        time.sleep(1);
-        
-        motorSerial.write(b"M");
-                
-        time.sleep(0.1);
-        
-        while(motorSerial.in_waiting > 0):
-            time.sleep(0.1);
-            print(motorSerial.readline().decode());
-            
-        time.sleep(10);
-        
-        print("Right");
-        setDirectionSpeed(motorSerial, "RIGHT", speed);
-        
-        time.sleep(1);
-        
-        motorSerial.write(b"M");
-                
-        time.sleep(0.1);
-        
-        while(motorSerial.in_waiting > 0):
-            time.sleep(0.1);
-            print(motorSerial.readline().decode());
-            
-        time.sleep(10);
+        print(motorSerial.readline().decode());
 
 except:
     pass;
--- Python/Motors.py
+++ Python/Motors.py
@@ -4,6 +4,21 @@ from Position import *;
 import time;
 
 
+# The angle-constant for rotating the robot.
+def angleConstant():
+    return(1);
+
+
+# The forward/backward-constant for moving the robot.
+def longitudinalConstant():
+    return(1);
+
+
+# The side-to-side-constant for moving the robot.
+def lateralConstant():
+    return(1);
+
+
 # Passes the speed of 0 to all motors.
 def stopMotors(motorSerial):
     setMotorSpeed(motorSerial, 0, 0, 0, 0);
@@ -13,16 +28,16 @@ def stopMotors(motorSerial):
 def setDirectionSpeed(motorSerial, direction, speed):
     if(direction == "RIGHT"):
         setMotorSpeed(motorSerial, speed, -1 * speed, -1 * speed, speed);
-        
+
     elif(direction == "FORWARD"):
         setMotorSpeed(motorSerial, -1 * speed, -1 * speed, speed, speed);
-        
+
     elif(direction == "BACKWARD"):
         setMotorSpeed(motorSerial, speed, speed, -1 * speed, -1 * speed);
-        
+
     elif(direction == "LEFT"):
         setMotorSpeed(motorSerial, -1 * speed, speed, speed, -1 * speed);
-        
+
     else:
         print("Invalid direction: {!r}".format(direction));
         setMotorSpeed(motorSerial, 0, 0, 0, 0);
@@ -31,24 +46,25 @@ def setDirectionSpeed(motorSerial, direction, speed):
 # Moves the robot in one of four predefined ways. Direction is local to the robot. Distance is in centimeters.
 def setDirectionPosition(motorSerial, direction, distance, speed):
     if(direction == "RIGHT"):
-        adjustedDistance = int(1 * distance);
+        adjustedDistance = int(lateralConstant() * distance);
         setMotorPositionDelta(motorSerial, adjustedDistance, -1 * adjustedDistance, -1 * adjustedDistance, adjustedDistance, speed);
-        
+
     elif(direction == "FORWARD"):
-        adjustedDistance = int(17.9772049042 * distance);
+        adjustedDistance = int(longitudinalConstant() * distance);
         setMotorPositionDelta(motorSerial, -1 * adjustedDistance, -1 * adjustedDistance, adjustedDistance, adjustedDistance, speed);
-        
+
     elif(direction == "BACKWARD"):
-        adjustedDistance = int(17.9772049042 * distance);
+        adjustedDistance = int(longitudinalConstant() * distance);
         setMotorPositionDelta(motorSerial, adjustedDistance, adjustedDistance, -1 * adjustedDistance, -1 * adjustedDistance, speed);
-        
+
     elif(direction == "LEFT"):
-        adjustedDistance = int(1 * distance);
+        adjustedDistance = int(lateralConstant() * distance);
         setMotorPositionDelta(motorSerial, -1 * adjustedDistance, adjustedDistance, adjustedDistance, -1 * adjustedDistance, speed);
-        
+
     else:
         print("Invalid direction: {!r}".format(direction));
         setMotorSpeed(motorSerial, 0, 0, 0, 0);
+
 
 # Sets all motors to their respective passed-in speeds.
 def setMotorSpeed(motorSerial, frontRightSpeed, backRightSpeed, backLeftSpeed, frontLeftSpeed):
@@ -61,25 +77,27 @@ def setMotorPosition(motorSerial, frPos, frSpeed, brPos, brSpeed, blPos, blSpeed
     string = "P " + str(frPos) + " " + str(frSpeed) + " " + str(brPos) + " " + str(brSpeed) + " " + str(blPos) + " " + str(blSpeed) + " " + str(flPos) + " " + str(flSpeed) + " ";
     writeSerialString(motorSerial, string);
 
+
 # Reads motor speed and position from the MegaPi and returns everything in a string.
 def readMotorData(motorSerial):
-    
+
     motorSerial.write(b"M");
-    
+
     time.sleep(0.1);
-    
+
     motorSpeeds = motorSerial.readline().decode().strip();
     motorPositions = motorSerial.readline().decode().strip();
-    
+
     return("Speed: " + str(motorSpeeds) + "\nPosition: " + str(motorPositions));
+
 
 # Reads the motor positons (and speeds) from the MegaPi and returns an array of positions.
 def getMotorPosition(motorSerial):
-    
+
     motorSerial.write(b"M");
-    
+
     time.sleep(0.1);
-    
+
     motorSpeeds = motorSerial.readline().decode().strip().split(',');
     motorPositions = motorSerial.readline().decode().strip().split(',');
     motorPositions[0] = int(motorPositions[0]);
@@ -97,14 +115,15 @@ def setMotorPositionDelta(motorSerial, frPos, brPos, blPos, flPos, allSpeed):
     newbrPos = brPos + currentPos[1];
     newblPos = blPos + currentPos[2];
     newflPos = flPos + currentPos[3];
-    
+
     setMotorPosition(motorSerial, newfrPos, allSpeed, newbrPos, allSpeed, newblPos, allSpeed, newflPos, allSpeed);
 
 
 # Rotates the robot by a specified angle and speed.
 def rotate(motorSerial, angle, speed):
-    realAngle = int(6.4809917355 * angle);
+    realAngle = int(angleConstant() * angle);
     setMotorPositionDelta(motorSerial, realAngle, realAngle, realAngle, realAngle, speed);
+
 
 # Utility method to write data to the robot.
 def writeSerialString(motorSerial, string):
