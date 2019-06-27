9b75d99c3acca5a0a2ba4b34ba899633b4332b31
Fletcher Easton <fletcheaston@gmail.com>
2019-06-27 16:13:31 -0700

    MotorTest revert.

--- Python/MotorTest.py
+++ Python/MotorTest.py
@@ -6,9 +6,6 @@ import serial;
 
 motorSerial = serial.Serial('/dev/ttyUSB0', 57600, timeout=0.1);
 
-dirs = ["FORWARD", "LEFT", "BACKWARD", "RIGHT"];
-counter = 0;
-timeCounter = 0;
 speed = 255;
 
 time.sleep(5);
@@ -19,18 +16,18 @@ while(motorSerial.in_waiting > 0):
 
 time.sleep(1);
 
+print("Forward");
+setDirectionPosition(motorSerial, "FORWARD", 100, 255);
+
+time.sleep(5);
+
 while(True):
-    timeCounter = time.time();
+    motorSerial.write(b"M");
 
-    setDirectionPosition(motorSerial, dirs[counter % 4], 100, 255);
+    time.sleep(0.1);
 
-    while(timeCounter < time.time() - 5):
-        motorSerial.write(b"M");
+    while(motorSerial.in_waiting > 0):
+        time.sleep(0.1);
+        print(motorSerial.readline().decode());
 
-        time.sleep(0.25);
-
-        while(motorSerial.in_waiting > 0):
-            time.sleep(0.1);
-            print(motorSerial.readline().decode().strip());
-
-    counter = counter + 1;
+    time.sleep(1);
